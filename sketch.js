fe5b0d5dde1d9f8388aac64622bd711c1d515506
MartynoSiela <m.satas@gmail.com>
2026-03-21 11:29:10 +0200

    Make circle move around canvas

--- sketch.js
+++ sketch.js
@@ -1,13 +1,19 @@
 function setup() {
   createCanvas(400, 400);
 
-  frameRate(2);
+  frameRate(30);
 }
 
 function draw() {
-  background(
-    Math.floor(Math.random() * 255), 
-    Math.floor(Math.random() * 255), 
-    Math.floor(Math.random() * 255)
-  );
+  background(220);
+
+  var circle_path_radius = 100;
+  var circle_path_center_x = 200;
+  var circle_path_center_y = 200;
+  var time = millis() / 1000;
+
+  var circle_center_x = circle_path_radius * cos(time) + circle_path_center_x;
+  var circle_center_y = circle_path_radius * sin(time) + circle_path_center_y;
+
+  circle(circle_center_x, circle_center_y, 100)
 }
