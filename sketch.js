504cde0cbef66b8994cc75724f329a13f8bc5768
MartynoSiela <m.satas@gmail.com>
2026-03-28 10:46:31 +0200

    Add Bouncable class

--- sketch.js
+++ sketch.js
@@ -1,19 +1,11 @@
+var test = new Bouncable(100, 100, 50);
+
 function setup() {
   createCanvas(400, 400);
-
-  frameRate(30);
 }
 
 function draw() {
   background(220);
 
-  var circle_path_radius = 100;
-  var circle_path_center_x = 200;
-  var circle_path_center_y = 200;
-  var time = millis() / 1000;
-
-  var circle_center_x = circle_path_radius * cos(time) + circle_path_center_x;
-  var circle_center_y = circle_path_radius * sin(time) + circle_path_center_y;
-
-  circle(circle_center_x, circle_center_y, 100)
+  test.draw();
 }
